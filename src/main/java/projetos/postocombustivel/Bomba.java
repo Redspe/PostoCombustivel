e4fa92258f8440211628d785a01c4dbf3e4b47db
Rafael Herrera <101949623+Redspe@users.noreply.github.com>
2024-04-09 16:12:01 -0300

    Revert "Simplificado"

--- src/main/java/projetos/postocombustivel/Bomba.java
+++ src/main/java/projetos/postocombustivel/Bomba.java
@@ -1,7 +1,6 @@
 package projetos.postocombustivel;
 
 import java.util.ArrayList;
-import static projetos.postocombustivel.PostoCombustivel.proxIdBomba;
 
 public class Bomba {
 
@@ -15,40 +14,7 @@ public class Bomba {
         this.faturamentoBomba = faturamentoBomba;
     }
 
-    /**
-     * Cadastra uma nova bomba de combustivel no sistema
-     *
-     * @param listaBombas
-     * @param listaTanques
-     */
-    public static void cadastrar(ArrayList<Bomba> listaBombas, ArrayList<Tanque> listaTanques) {
-        if (!listaTanques.isEmpty()) {
-
-            IO.println("\nEscolha um tanque: ");
-            Tanque.listar(listaTanques); // Lista todos os tanques disponiveis
-
-            String msg = "\nDigite o número do tanque escolhido (ou 0 para sair): ";
-            String errorMsg = "Número inválido! Escolha um dos números disponívies acima.";
-            int opc = IO.chooseInRange(0, listaTanques.size(), msg, errorMsg) - 1;
-            if (opc == -1) {
-                return;
-            }
-
-            Tanque tanqueEscolhido = listaTanques.get(opc);
-
-            Bomba novaBomba = new Bomba(proxIdBomba(), tanqueEscolhido, 0);
-            listaBombas.add(novaBomba);
-
-            IO.println("\nNova bomba cadastrada com sucesso!");
-            IO.aperteContinuar();
-        } else {
-            IO.println("\nNão há nenhum tanque cadastrado no momento!");
-            IO.aperteContinuar();
-        }
-
-    }
-
-    public static void listar(ArrayList<Bomba> listaBombas) {
+    public static void listarBombas(ArrayList<Bomba> listaBombas) {
         for (int i = 0; i < listaBombas.size(); i++) {
             IO.println("\nBomba " + listaBombas.get(i).getId() + ":");
             IO.println("  Tipo: " + listaBombas.get(i).getTanque().getTipo().getTipo());
@@ -59,7 +25,7 @@ public class Bomba {
     public static void abastecerPorLitro(ArrayList<Bomba> listaBombas, int bomba) {
         double precoLitro = listaBombas.get(bomba).getTanque().getTipo().getPreco();
 
-        listar(listaBombas);
+        listarBombas(listaBombas);
 
         double qtdAbastecer = IO.readDouble("\nDigite a quantidade de litros a abastecer: ");
 
@@ -86,7 +52,7 @@ public class Bomba {
     public static void abastecerPorValor(ArrayList<Bomba> listaBombas, int bomba) {
         double precoLitro = listaBombas.get(bomba).getTanque().getTipo().getPreco();
 
-        listar(listaBombas);
+        listarBombas(listaBombas);
 
         double valorAbastecer = IO.readDouble("\nDigite o valor a abastecer: R$ ");
         double qtdAbastecer = valorAbastecer / precoLitro;
